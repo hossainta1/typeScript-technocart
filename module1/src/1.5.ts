c798b8a48e33ab27abfd3409d5b7612fb3e5c597
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-05-01 00:34:35 +0600

    Referance tyoe object

---
+++ module1/src/1.5.ts
@@ -0,0 +1,13 @@
+// Referance type ------> object
+
+const user: {
+   readonly company: "Hajee Air"; // type. And this type is called literal type / using Assigning Fixed value Which can be Chanaged
+  firstName: string;
+  lastName?: string; //Optional  type
+  age: number;
+} = {
+  company: "Hajee Air",
+  firstName: "Tanvir Hossen",
+  lastName: "Hossain",
+  age: 19,
+};
---
+++ module1/src/1.6.ts
@@ -0,0 +1,5 @@
+// Learning function
+
+// Normal Function
+
+// Arrow function
