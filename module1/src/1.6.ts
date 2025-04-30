643c5887860ca513375c1b6c937ab5cbfddbb407
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-05-01 00:52:15 +0600

    TypeScript Function

--- module1/src/1.6.ts
+++ module1/src/1.6.ts
@@ -3,3 +3,33 @@
 // Normal Function
 
 // Arrow function
+
+function add(num1: number, num2: number) : number {
+  return num1 + num2;
+}
+
+add(12, 24);
+
+// Arrow function
+
+const addArrow = (num1 : number, num2 : number) : number => num1 + num2;
+
+
+// in object if any function has then it is a method
+
+const poorUser = {
+    name: 'Tanvir',
+    balance: 0,
+    addBalance(balance: number): number {
+
+        return this.balance + balance
+
+    }
+}
+
+
+// Callback funtion
+
+const arr : number[] = [1, 2, 3];
+
+const newArray : number[] = arr.map((element : number) : number => element * element)
