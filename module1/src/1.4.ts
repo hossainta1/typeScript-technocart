ac424b00b31cc01feb41fb7220b1f577676fc08c
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-04-30 23:55:26 +0600

    Different types of Data type

--- module1/src/1.4.ts
+++ module1/src/1.4.ts
@@ -1,1 +1,39 @@
 // Basic dat type
+
+// Srting
+ let firstName: string = "tanvir";
+
+//  number
+
+let roll: number = 123;
+
+// bollean
+
+let isAdmin : boolean = true;
+
+// undefine
+
+let x : undefined= undefined;
+
+// null
+
+let y: null= null;
+
+// Define any type (Aboid to use any type data type decleration)
+
+let d;
+
+// Array
+
+let friendsName: string[] = ["Robin", "Muzee"];
+
+let validRoll :number[]= [12, 25];
+
+// Tuple (define type of valy oder in array)
+
+let coordinator: [number, number] = [12, 5]
+
+let ageName : [number, string] = [12, "TAnvir"]
+
+
+
