86d0fb53cdc3149c8e4816b33f4ed74c75e92dc0
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-05-02 20:40:05 +0600

    Interface and type alias

---
+++ module2/2.2.interface.ts
@@ -0,0 +1,50 @@
+{
+  //
+
+  type User1 = {
+    name: string;
+    age: number;
+  };
+
+  interface User2 {
+    name: string;
+    age: number;
+  }
+
+  type UserWithRoll1 = User1 & { role: string };
+
+  interface UserWithRoll2 extends User2 {
+    role: string;
+  }
+
+  const user1: UserWithRoll2 = {
+    name: "Tanvir",
+    age: 25,
+    role: "manager",
+  };
+
+  // Interface in Array
+
+  type Roll1 = number[];
+
+  // Aray in interface
+  interface Roll2 {
+    [index: number]: number;
+  }
+
+  const rollNumber: Roll2 = [1, 2, 3, 4];
+
+  
+
+  // Function with interface alos shoe the alias type
+
+  type Add1 = (num1: number, num2: number) => number;
+
+  interface Add2 {
+    (num1: number, num2: number): number;
+  }
+
+  const add: Add2 = (num1, num2) => num1 + num2;
+
+  //
+}
