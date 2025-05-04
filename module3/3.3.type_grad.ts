4dc24e54ab9c6d82cf01dd959fd44e7455afeaa5
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-05-04 23:30:11 +0600

    type gard

---
+++ module3/3.3.type_grad.ts
@@ -0,0 +1,68 @@
+{
+  //tyape gard
+
+  // How typeOf work on a type gard
+
+  type Alphanumeric = number | string;
+
+  const add = (param1: Alphanumeric, param2: Alphanumeric): Alphanumeric => {
+    if (typeof param1 === "number" && param2 === "number") {
+      return param1 + param2;
+    } else {
+      return param1.toString() + param2.toString();
+    }
+  };
+
+  const result1 = add(2, 3);
+  console.log(result1);
+  //
+
+  /// in gard
+  type NormalUser = {
+    name: string;
+  };
+
+  type AdminUSer = {
+    name: string;
+    role: "admin";
+  };
+
+  const getUser = (user: NormalUser | AdminUSer) => {
+    if ("role" in user) {
+      console.log(`My name is ${user.name} & my role is ${user.role}`);
+    }else{
+        console.log(`My name is ${user.name} & my role is ${user}`);
+    }
+  };
+
+  const normalUser: NormalUser = {
+    name : 'Mr Tanvir'
+  }
+
+
+  const adminUser: AdminUSer = {
+    name: 'Hossen',
+    role: "admin"
+
+  }
+
+  getUser(normalUser)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
